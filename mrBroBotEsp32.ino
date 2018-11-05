a4b10a145ec9c00320ce58eec1b84b8bc5fb31f2
Fedor Sokovikov <FedorkZ@gmail.com>
2018-11-05 03:33:59 +0300

    simple wifi with UDP announce

--- mrBroBotEsp32.ino
+++ mrBroBotEsp32.ino
@@ -3,6 +3,15 @@
 #define LOG_DMP false
 #define LOG_LOOP false
 #define PID_LOG false
+#define LOG_WIFI false
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
+//#include <WiFiUdp.h>
+
+#include "AsyncUDP.h"
 
 #include "PID_v1.h"
 #include "LMotorController.h"
@@ -12,6 +21,22 @@
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
 #include "Wire.h"
 #endif
+
+const char* ssid = "HomeSweetHome";
+const char* password = "allUneedisLOvE";
+
+WebServer server(80);
+
+AsyncUDP udp;
+
+// Multicast declarations
+IPAddress ipBroadcast(255, 255, 255, 255);
+unsigned int portBroadcast = 61565; // port to broadcast on must be between1024...65000
+bool gotFirstCommand = false;
+unsigned long lastTimeIpSent = 0;
+char ipAddressBuff[255];
+const int led = 13;
+bool needResetFIFO = false;
 
 #define MIN_ABS_SPEED 1
 
@@ -30,7 +55,6 @@ Quaternion q;           // [w, x, y, z]         quaternion container
 VectorFloat gravity;    // [x, y, z]            gravity vector
 float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 
-//PID
 double originalSetpoint = 246.5;
 double setpoint = originalSetpoint;
 double movingAngleOffset = 0.1;
@@ -44,6 +68,11 @@ PID pid(&input, &output, &setpoint, Kp, Ki, Kd, DIRECT);
 
 double motorSpeedFactorLeft = 1;
 double motorSpeedFactorRight = 1;
+
+bool operating = true;
+double leftShift = 0.0;
+double rightShift = 0.0;
+int minAbsSpeed = 20;
 
 // PINOUTS
 
@@ -79,9 +108,53 @@ void dmpDataReady()
     mpuInterrupt = true;
 }
 
+void setupWifi(){
+  Serial.println("WIFI SETUP");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (MDNS.begin("esp32")) {
+    Serial.println("MDNS responder started");
+  }
+
+  IPAddress ip = WiFi.localIP();
+  sprintf(ipAddressBuff, "mrBRObot:IP=%d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3]);
+
+  Serial.print("UDP BROADCAST: ");
+  Serial.println(ipAddressBuff);
+  Serial.println("Server setup");
+
+  server.on("/inline", []() {
+    server.send(200, "text/plain", "this works as well");
+  });
+  
+  server.on("/ctrl", ctrlHandleFunc);
+  server.on("/", ctrlHandleFunc);
+    
+  server.begin();
+  Serial.println("HTTP server started");
+  Serial.println("  --= SETUP DONE =--");
+
+
+}
 
 void setup()
 {
+  Serial.begin(115200);
+
+  setupWifi();
   delay(3000);
     // join I2C bus (I2Cdev library doesn't do this automatically)
     #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
@@ -94,7 +167,6 @@ void setup()
     // initialize serial communication
     // (115200 chosen because it is required for Teapot Demo output, but it's
     // really up to you depending on your project)
-    Serial.begin(115200);
     while (!Serial); // wait for Leonardo enumeration, others continue immediately
 
     // initialize device
@@ -158,6 +230,25 @@ void setup()
 
 void loop()
 {
+  server.handleClient(); //this is required for handling the incoming requests
+
+    if (needResetFIFO){
+      needResetFIFO = false;
+      mpu.resetFIFO();
+      fifoCount = 0;
+      mpu.setDMPEnabled(true);
+    }
+    
+    if (!gotFirstCommand){
+      unsigned long now = millis();
+            
+      if (now >= lastTimeIpSent + 2000){      
+        Serial.println("Sending IP addres via UDP"); 
+        lastTimeIpSent = now;
+        sendIpAddressViaUDP();
+      }
+    }
+  
       #if LOG_LOOP
       Serial.print(F("."));
       #endif
@@ -181,12 +272,27 @@ void loop()
     {
         //no mpu data - performing PID calculations and output to motors
         pid.Compute();
+
+        double lSpeed = output + leftShift;
+        double rSpeed = output + rightShift;
         #if LOG_OUTPUT
         Serial.print(F("output "));
-        Serial.println(output);
+        Serial.print(output);
+        Serial.print(" lshift: ");
+        Serial.print(leftShift);
+        Serial.print(" lshift: ");
+        Serial.print(rightShift);
+        Serial.print(" l: ");
+        Serial.print(lSpeed);
+        Serial.print(" r: ");
+        Serial.println(rSpeed);
         #endif
-        motorController.move(output, MIN_ABS_SPEED);
-       
+
+        if (operating) {
+          motorController.move(lSpeed, rSpeed, minAbsSpeed);
+        } else {
+          motorController.move(0, 0, 0);
+        }       
     }
     
     // reset interrupt flag and get INT_STATUS byte
@@ -235,4 +341,131 @@ void loop()
             Serial.print("\t");
         #endif
    }
+}
+
+///// WIFI
+void ctrlHandleFunc(){
+   #if LOG_WIFI
+   Serial.println("GOT request:");
+   #endif
+   mpu.setDMPEnabled(false);
+   gotFirstCommand = true;
+   if (server.args() > 0){
+    for (int i = 0; i < server.args();i++){
+
+        String key = server.argName(i);
+        String val = server.arg(i);
+
+        #if LOG_WIFI
+        Serial.print("Name: "); Serial.println(key);
+        Serial.print("Value: "); Serial.println(val);
+        #endif
+        
+        handleParam(key, val);
+      }
+   }
+   server.send(200, "text/html", "OK");
+   #if LOG_WIFI
+   Serial.println("END request");
+   #endif
+   needResetFIFO = true;
+}
+
+String KP = "kp";
+String KI = "ki";
+String KD = "kd";
+String LEFT_SHIFT = "lshift";
+String RIGHT_SHIFT = "rshift";
+String TARGET = "target";
+String LEFT_COEF = "lcoef";
+String RIGHT_COEF = "rcoef";
+String MINSTEP = "minstep";
+String SAMPLETIME = "sampletime";
+String OPERATE = "operate";
+
+void handleParam(String key, String val){
+  val.trim();
+  
+  if (val.length() == 0) {
+    #if LOG_WIFI    
+    Serial.print(key); Serial.print(" -> "); Serial.println("DROPPED EMPTY"); 
+    #endif
+    return;
+  }
+    
+  if (KP.equalsIgnoreCase(key)){
+    Kp = val.toFloat();
+    pid.SetTunings(Kp, Ki, Kd);
+    #if LOG_WIFI
+    Serial.print("Kp -> "); Serial.println(Kp); 
+    #endif
+  } else if (KI.equalsIgnoreCase(key)){
+    Ki = val.toFloat();
+    pid.SetTunings(Kp, Ki, Kd);
+    #if LOG_WIFI
+    Serial.print("Ki -> "); Serial.println(Ki); 
+    #endif
+  } else if (KD.equalsIgnoreCase(key)){
+    Kd = val.toFloat();
+    pid.SetTunings(Kp, Ki, Kd);
+    #if LOG_WIFI
+    Serial.print("Kd -> "); Serial.println(Kd); 
+    #endif
+  } else if (SAMPLETIME.equalsIgnoreCase(key)){
+    int stime = val.toInt();
+    if (stime >= 1){
+       pid.SetSampleTime(stime);
+    }
+    #if LOG_WIFI
+    Serial.print("stime -> "); Serial.println(stime); 
+    #endif
+  } else if (LEFT_SHIFT.equalsIgnoreCase(key)){
+    leftShift = val.toFloat();
+    #if LOG_WIFI
+    Serial.print("leftShift -> "); Serial.println(leftShift); 
+    #endif
+  } else if (RIGHT_SHIFT.equalsIgnoreCase(key)){
+    rightShift = val.toFloat();
+    #if LOG_WIFI
+    Serial.print("rightShift -> "); Serial.println(rightShift); 
+    #endif
+  } else if (TARGET.equalsIgnoreCase(key)){
+    setpoint = val.toFloat();
+    #if LOG_WIFI
+    Serial.print("originalSetpoint -> "); Serial.println(setpoint); 
+    #endif
+  } else if (RIGHT_COEF.equalsIgnoreCase(key)){
+    motorSpeedFactorRight = val.toFloat();
+    #if LOG_WIFI
+    Serial.print("motorSpeedFactorRight -> "); Serial.println(motorSpeedFactorRight); 
+    #endif
+  } else if (LEFT_COEF.equalsIgnoreCase(key)){
+    motorSpeedFactorLeft = val.toFloat();
+    #if LOG_WIFI
+    Serial.print("motorSpeedFactorLeft -> "); Serial.println(motorSpeedFactorLeft); 
+    #endif
+  } else if (MINSTEP.equalsIgnoreCase(key)){
+    minAbsSpeed = val.toInt();
+    #if LOG_WIFI
+    Serial.print("minAbsSpeed -> "); Serial.println(minAbsSpeed); 
+    #endif
+  } else if (OPERATE.equalsIgnoreCase(key)){
+    operating = val.toInt() != 0;
+    #if LOG_WIFI
+    Serial.print("operating -> "); Serial.println(operating); 
+    #endif
+  } else {
+    #if LOG_WIFI
+    Serial.print(key); Serial.print(" -> "); Serial.println("IGNORED"); 
+    #endif
+  }
+}
+
+/// UPD
+void sendIpAddressViaUDP() {
+  udp.broadcastTo("mrBRObot:IP=", 1234);
+//  Udp.beginPacket(ipBroadcast, portBroadcast);
+//  Udp.write("mrBRObot:IP=");
+//  Udp.write(ipAddressBuff);
+//  Udp.endPacket(); 
 }
